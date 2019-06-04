e35a13965790d0e3c5bbf97c083259447ba9c1b4
Jeremy Reyes B <jjrb6@hotmail.com>
2019-06-04 12:50:23 -0500

    [feat]: Create tasks log model.

---
+++ models/task-log.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+/**
+ * Config schema tasks
+ */
+let taskLogSchema = new Schema({
+    start: {
+        type: Date,
+        required: true
+    },
+    pause: {
+        type: Date,
+        required: false
+    },
+    task: {
+        type: Schema.Types.ObjectId,
+        ref: 'tasks'
+    }
+});
+
+
+module.exports = mongoose.model('tasksLog', taskLogSchema);
