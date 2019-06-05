4ddd210e8774fe6771912f6fa8eb0749bb7c00af
Jeremy Reyes B <jjrb6@hotmail.com>
2019-06-05 17:11:41 -0500

    [test]: Login test password no correct.

--- __test__/user/login.test.js
+++ __test__/user/login.test.js
@@ -28,6 +28,32 @@ describe('Login', () => {
             });
     });
 
+
+    test('Password is not correct', (done) => {
+        request(app)
+            .post("/api/login")
+            .set('Accept', 'application/json')
+            .send({
+                email: 'jjrb6@hotmail.com',
+                password: '123456'
+            })
+            .expect(400)
+            .end((err, res) => {
+
+                if (err) return done(err);
+
+                expect(res.body).toStrictEqual({
+                    success: false,
+                    err: {
+                        message: 'User and password are not correct'
+                    }
+                });
+
+                done();
+            });
+    });
+
+
     test('Login without email', (done) => {
         request(app)
             .post("/api/login")
