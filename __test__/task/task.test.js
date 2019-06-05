a1a30b460a23f8204e2494eac32d5acd19626d41
Jeremy Reyes B <jjrb6@hotmail.com>
2019-06-05 15:56:24 -0500

    [test]: Init, pause and restart task.

--- __test__/task/task.test.js
+++ __test__/task/task.test.js
@@ -5,7 +5,7 @@ const app = require('../../server');
 process.env.NODE_ENV = 'dev';
 
 
-describe('Create new task', () => {
+describe('Tasks and their functionalities', () => {
 
     let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhVXNlciI6eyJzdGF0dXMiOnRydWUsIl9pZCI6IjVjZjJlZjQ3Y2E0NzY3MDgzMmQyNWUzYyIsIm5hbWUiOiJKZXJlbXkgUmV5ZXMgQi4iLCJlbWFpbCI6ImpqcmI2QGhvdG1haWwuY29tIiwiX192IjowfSwiaWF0IjoxNTU5NzYxNzYxLCJleHAiOjE1NTk5MzQ1NjF9.4MpvmoMe3eSDzrI65jA3DiO-3_qxj9Q64--y9yY-Pcg';
 
@@ -101,6 +101,117 @@ describe('Create new task', () => {
                     success: false,
                     message: 'Invalid format duration'
                 });
+
+                done();
+            });
+    });
+
+
+    test('Init task', (done) => {
+        request(app)
+            .post("/api/init-task/")
+            .set('Authorization', token)
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .expect(200)
+            .send({
+                task_id: '5cf6d1e59c95f5224404917b'
+            })
+            .end((err, res) => {
+
+                if (err) return done(err);
+
+                expect(res.body.success).toEqual(true);
+                expect(typeof res.body.task).toBe('object');
+
+                done();
+            });
+    });
+
+
+    test('The task is already started', (done) => {
+        request(app)
+            .post("/api/init-task/")
+            .set('Authorization', token)
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .expect(400)
+            .send({
+                task_id: '5cf6d1e59c95f5224404917b'
+            })
+            .end((err, res) => {
+
+                if (err) return done(err);
+
+                console.log(res.body);
+
+                expect(res.body).toStrictEqual({
+                    success: false,
+                    message: 'The task is already started'
+                });
+
+                done();
+            });
+    });
+
+
+    test('Pause task', (done) => {
+        request(app)
+            .post("/api/pause-task/")
+            .set('Authorization', token)
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .expect(200)
+            .send({
+                task_id: '5cf6d1e59c95f5224404917b'
+            })
+            .end((err, res) => {
+
+                if (err) return done(err);
+
+                expect(res.body.success).toEqual(true);
+                expect(typeof res.body.task).toBe('object');
+
+                done();
+            });
+    });
+
+
+    test('Pause task', (done) => {
+        request(app)
+            .post("/api/pause-task/")
+            .set('Authorization', token)
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .expect(400)
+            .send({
+                task_id: '5cf6d1e59c95f5224404917b'
+            })
+            .end((err, res) => {
+
+                if (err) return done(err);
+
+                expect(res.body).toStrictEqual({
+                    success: false,
+                    message: 'The task is already paused'
+                });
+
+                done();
+            });
+    });
+
+
+    test('Restart task', (done) => {
+        request(app)
+            .post("/api/restart-task/")
+            .set('Authorization', token)
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .expect(200)
+            .send({
+                task_id: '5cf6d1e59c95f5224404917b'
+            })
+            .end((err, res) => {
+
+                if (err) return done(err);
+
+                expect(res.body.success).toEqual(true);
+                expect(typeof res.body.task).toBe('object');
 
                 done();
             });
